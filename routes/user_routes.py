71dbf30d2a618670407b651c017c6bacce3fa5ac
Safal Lama <140276215+RyuZinOh@users.noreply.github.com>
2024-11-16 00:25:43 +0545

    backend [CORS ] fix - patch1

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, request, jsonify
+from flask_cors import CORS 
 from services.user_service import register_user, login_user
 
 user_bp = Blueprint('user', __name__, url_prefix='/user')
+
+# CORS REQ verification for routes api
+CORS(user_bp)
 
 @user_bp.route('/register', methods=['POST'])
 def register():
