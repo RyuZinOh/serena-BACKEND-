227835e56b93b0d7e41eeaee50f9666c03627619
ryuzinoh <whysafalisop@gmail.com>
2024-12-11 15:05:23 +0545

    adding fetching logo option

--- routes/currency_users.py
+++ routes/currency_users.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_cors import CORS
 import jwt
-from services.currency_service import get_user_currency
+from services.currency_service import get_user_currency, get_user_photo
 from config import Config
 
 currency_bp = Blueprint('currency', __name__, url_prefix='/currency')
@@ -29,3 +29,26 @@ def get_currency(user_id):
         return jsonify({'message': 'Currency data not found for this user!'}), 404
 
     return jsonify(currency_data), 200
+
+
+@currency_bp.route('/<user_id>/photo', methods=['GET'])
+def get_photo(user_id):
+ 
+    token = request.headers.get('Authorization', "").replace("Bearer ", "")
+    if not token:
+        return jsonify({'message': 'Token is missing!'}), 401
+
+    try:
+        decoded_token = jwt.decode(token, Config.SECRET_KEY, algorithms=['HS256'])
+        logged_in_user_id = decoded_token.get('sub')
+    except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+        return jsonify({'message': 'Invalid or expired token!'}), 401
+
+    if logged_in_user_id != user_id:
+        return jsonify({'message': 'Unauthorized!'}), 403
+
+    photo_data = get_user_photo(user_id)
+    if not photo_data:
+        return jsonify({'message': 'Photo not found for this user!'}), 404
+
+    return jsonify(photo_data), 200
--- services/currency_service.py
+++ services/currency_service.py
@@ -13,3 +13,16 @@ def get_user_currency(user_id):
         }
     
     return None
+
+def get_user_photo(user_id):
+    user_data = mongo.db.users.find_one(
+        {"user_id": user_id},
+        {"photo": 1, "_id": 0}  
+    )
+    
+    if user_data and "photo" in user_data:
+        return {
+            "photo": user_data["photo"]
+        }
+    
+    return None
